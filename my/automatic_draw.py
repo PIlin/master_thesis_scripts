0db90f2934b14698c4b73f60b4bfe7cc57679267
Pavel Ilin <ilin.pa@gmail.com>
2013-11-20 01:09:18 +0200

    draw jitter

--- my/automatic_draw.py
+++ my/automatic_draw.py
@@ -31,50 +31,91 @@ matplotlib.rcParams.update(pgf_with_pdflatex)
 import matplotlib.pyplot as plt 
 
 
+data = {}
+files = ('tps_ack', 'tps_noack', 'jts_ack', 'jts_noack')
+for fn in files:
+	fname = fn + '.data'
+	with open(fname, "rb") as f:
+		data[fn] = pickle.load(f)
 
-with open("tps.data", "rb") as f:
-	tps = pickle.load(f)
 
-with open("jts.data", "rb") as f:
-	jts = pickle.load(f)
 
 
 # print(tps)
 
-def new_figure():
+def new_figure(figsize = (6,4)):
 	# figsize = (8,6)
-	figsize = (6,4.5)
+	# figsize = (6,4.5)
 
 	f = plt.figure(figsize=figsize)
 
-	# f.tight_layout(.5)
+	f.set_tight_layout(1)
 
 	return f
 
 
-def draw_throughput(fig, data):
+def draw_throughput(ax, data, style, label):
 	x = range(1, len(data) + 1)
 
 	print(len(x), len(data))
 
 	d = [d / 1000 for d in data]
 
-	fig.plot(x, d, 'k+-')
-	fig.grid()
+	ax.plot(x, d, style, markersize = 2.3, label = label)
 
-	fig.set_xlabel(u"Розмір корисного навантаження пакету, байт")
-	fig.set_ylabel(u"Корисна пропускна здібність, Кбіт/с")
+	ax.set_xlabel(u"Розмір корисного навантаження пакету, байт")
+	ax.set_ylabel(u"Корисна пропускна здібність, Кбіт/с")
+
+def draw_jitter(ax, data):
+
+	print(len(data))
+
+	idx = [0]
+	idx.extend(range(19,120,20))
 
 
+	print(idx)
 
+	d = [[x * 1000 for x in data[i]] for i in idx]
+
+	bp = ax.boxplot(d,0,'')
+	plt.xticks(range(1, len(idx) + 1), [i + 1 for i in idx])
+
+	# pprint(bp)
+
+	plt.setp(bp['boxes'], color = 'black')
+	plt.setp(bp['whiskers'], color = 'black')
+	plt.setp(bp['medians'], color = 'black')
+
+	ax.set_xlabel(u"Розмір корисного навантаження пакету, байт")
+	ax.set_ylabel(u"Затримка передачі, мс")
 
 
 # print (f.get_size_inches())
 
 f = new_figure()
 ax = f.add_subplot(111)
-draw_throughput(ax, tps)
-f.savefig('fig.pdf')
-f.savefig('fig.pgf')
+draw_throughput(ax, data['tps_ack'], 'ko-', u'Із ACK')
+draw_throughput(ax, data['tps_noack'], 'k^-', u'Без ACK')
+ax.legend(loc = 0)
+ax.grid()
+f.savefig('tp.pdf')
+f.savefig('tp.pgf')
+
+f = new_figure((4,4))
+ax = f.add_subplot(111)
+draw_jitter(ax, data['jts_ack'])
+ax.set_ylim(0,16)
+f.savefig('jts_ack.pdf')
+f.savefig('jts_ack.pgf')
+
+
+f = new_figure((4,4))
+ax = f.add_subplot(111)
+draw_jitter(ax, data['jts_noack'])
+ax.set_ylim(0,16)
+f.savefig('jts_noack.pdf')
+f.savefig('jts_noack.pgf')
+
 
 # plt.show()
